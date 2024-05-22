fde84b9c2186e3716d46787a9bb09755e2f74a09
vic_chiu <vic_chiu@simbo.com.tw>
2024-05-22 13:53:26 +0800

    同一份檔案新增多筆describe

--- __tests__/onelogin.test.js
+++ __tests__/onelogin.test.js
@@ -27,13 +27,12 @@ afterAll(async () => {
   await browser.close();
 });
 
-describe('Login Test', () => {
+describe('檢查左側目錄', () => {
   test('檢查admin 左邊選單存在管理員設定', async () => {
     await page.waitForSelector('#root > div > div > div > aside > div > div._menu_t2mh1_44 > ul > li:nth-child(1) > div', { timeout: 60000 });
     const adminSettingText = await page.$eval('#root > div > div > div > aside > div > div._menu_t2mh1_44 > ul > li:nth-child(1) > div', element => element.textContent.trim());
     assert.equal(adminSettingText, '管理員設定', 'Admin setting element text is incorrect');
   });
-
   test('檢查admin 左邊選單存在會員管理', async () => {
     const memberManagementText = await page.$eval('#root > div > div > div > aside > div > div._menu_t2mh1_44 > ul > li:nth-child(2) > div > span', element => element.textContent.trim());
     assert.equal(memberManagementText, '會員管理', 'Member management element text is incorrect');
@@ -47,5 +46,46 @@ describe('Login Test', () => {
     assert.equal(reportText, '統計報表', 'report Textelement text is incorrect');
   });
 });
-
+describe('檢查上方目錄', () => {
+  test('檢查admin上方目錄登入名稱', async () => {
+      await page.waitForSelector('#root > div > div > div > aside > div > div._menu_t2mh1_44 > ul > li:nth-child(1) > div', { timeout: 60000 });
+      const adminSettingText = await page.$eval('#root > div > div > div > aside > div > div._menu_t2mh1_44 > ul > li:nth-child(1) > div', element => element.textContent.trim());
+      assert.equal(adminSettingText, '管理員設定', 'Admin setting element text is incorrect');
+  });
+  test('檢查admin上方目錄金額', async () => {
+      const memberManagementText = await page.$eval('#root > div > div > div > aside > div > div._menu_t2mh1_44 > ul > li:nth-child(2) > div > span', element => element.textContent.trim());
+      assert.equal(memberManagementText, '會員管理', 'Member management element text is incorrect');
+  });
+  test('檢查admin上方目錄切換語系', async () => {
+      const onlineplayerText = await page.$eval('#root > div > div > div > aside > div > div._menu_t2mh1_44 > ul > li.ant-menu-item > span > a', element => element.textContent.trim());
+      assert.equal(onlineplayerText, '在線玩家', 'online player element text is incorrect');
+  });
+  test('檢查admin上方目錄登出按鈕', async () => {
+      const reportText = await page.$eval('#root > div > div > div > aside > div > div._menu_t2mh1_44 > ul > li:nth-child(4) > div > span', element => element.textContent.trim());
+      assert.equal(reportText, '統計報表', 'report Textelement text is incorrect');
+  });
+});
+describe('檢查畫面中間區塊', () => {
+  test('檢查admin中間區塊-壓碼量', async () => {
+      await page.waitForSelector('#root > div > div > div > aside > div > div._menu_t2mh1_44 > ul > li:nth-child(1) > div', { timeout: 60000 });
+      const adminSettingText = await page.$eval('#root > div > div > div > aside > div > div._menu_t2mh1_44 > ul > li:nth-child(1) > div', element => element.textContent.trim());
+      assert.equal(adminSettingText, '管理員設定', 'Admin setting element text is incorrect');
+  });
+  test('檢查admin中間區塊-玩家淨輸贏', async () => {
+      const memberManagementText = await page.$eval('#root > div > div > div > aside > div > div._menu_t2mh1_44 > ul > li:nth-child(2) > div > span', element => element.textContent.trim());
+      assert.equal(memberManagementText, '會員管理', 'Member management element text is incorrect');
+  });
+  test('檢查admin中間區塊-同時在線人數', async () => {
+      const onlineplayerText = await page.$eval('#root > div > div > div > aside > div > div._menu_t2mh1_44 > ul > li.ant-menu-item > span > a', element => element.textContent.trim());
+      assert.equal(onlineplayerText, '在線玩家', 'online player element text is incorrect');
+  });
+  test('檢查admin中間區塊-每小時累計壓碼量', async () => {
+      const reportText = await page.$eval('#root > div > div > div > aside > div > div._menu_t2mh1_44 > ul > li:nth-child(4) > div > span', element => element.textContent.trim());
+      assert.equal(reportText, '統計報表', 'report Textelement text is incorrect');
+  });
+  test('檢查admin中間區塊-每小時玩家輸贏狀況', async () => {
+    const reportText = await page.$eval('#root > div > div > div > aside > div > div._menu_t2mh1_44 > ul > li:nth-child(4) > div > span', element => element.textContent.trim());
+    assert.equal(reportText, '統計報表', 'report Textelement text is incorrect');
+});
+});
 
