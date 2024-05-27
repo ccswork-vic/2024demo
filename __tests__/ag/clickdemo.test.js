10812a6aa509b3d2ca98d72acd65a9ee750e8cc6
vic_chiu <vic_chiu@simbo.com.tw>
2024-05-27 11:52:06 +0800

    點擊子目錄練習

--- __tests__/ag/clickdemo.test.js
+++ __tests__/ag/clickdemo.test.js
@@ -27,137 +27,13 @@ beforeAll(async () => {
 afterAll(async () => {
   await browser.close();
 });
-describe.skip('會員管理子目錄', () => {
-  test('確認新增會員是否存在', async () => {
-      await page.waitForSelector('#root > div > div > div > aside > div > div._menu_t2mh1_44 > ul > li:nth-child(1) > div', { timeout: 60000 });
-      await page.click('#root > div > div > div > aside > div > div._menu_t2mh1_44 > ul > li:nth-child(1) > div'); // 點會員管理
-      // 等待子菜单展開
-      await new Promise(resolve => setTimeout(resolve, 1000)); // 等待1秒鐘，確保子菜單展開
-      // 檢查新增會員是否存在
-      const createaccountExists = await page.evaluate(() => {
-        const links = document.querySelectorAll('a');
-        let createaccountFound = false;
-        links.forEach(link => {
-          if (link.textContent.trim() === '新增會員') {
-            createaccountFound = true;
-          }
-        });
-        return createaccountFound;
-      });
-  
-      // 斷言新增會員存在
-      expect(createaccountExists).toBeTruthy();
-      //assert.ok(memberListExists, '會員清單不存在');
-      //assert.equal(memberListExists, true, '會員清單不存在'); 
-    });
-  test('確認會員清單是否存在', async () => {
-    await new Promise(resolve => setTimeout(resolve, 1000)); // 等待1秒鐘，確保子菜單展開
-    // 檢查會員清單是否存在
-    const memberListExists = await page.evaluate(() => {
-      const links = document.querySelectorAll('a');
-      let memberListFound = false;
-      links.forEach(link => {
-        if (link.textContent.trim() === '會員清單') {
-          memberListFound = true;
-        }
-      });
-      return memberListFound;
-    });
-    // 斷言會員清單存在
-    expect(memberListExists).toBeTruthy();
-  });
-  test('確認新增子帳號是否存在', async () => {
-    await new Promise(resolve => setTimeout(resolve, 1000)); // 等待1秒鐘，確保子菜單展開
-    // 檢查新增子帳號是否存在
-    const newSubAccountExists = await page.evaluate(() => {
-      const links = document.querySelectorAll('a');
-      let newSubAccountFound = false;
-      links.forEach(link => {
-        if (link.textContent.trim() === '新增子帳號') {
-            newSubAccountFound = true;
-        }
-      });
-      return newSubAccountFound;
-    });
-    expect(newSubAccountExists).toBeTruthy();
-  });
-  test('確認子帳號清單是否存在', async () => {
-    await new Promise(resolve => setTimeout(resolve, 1000)); // 等待1秒鐘，確保子菜單展開
-    // 檢查子帳號清單是否存在
-    const subAccountListExists = await page.evaluate(() => {
-      const links = document.querySelectorAll('a');
-      let subAccountListFound = false;
-      links.forEach(link => {
-        if (link.textContent.trim() === '子帳號清單') {
-            subAccountListFound = true;
-        }
-      });
-      return subAccountListFound;
-    });
-    expect(subAccountListExists).toBeTruthy();
-  });
-});
-// describe('會員管理測試', () => {
-//     test('確認新增會員、會員清單、新增子帳號和子帳號清單是否存在', async () => {
-//       await page.waitForSelector('#root > div > div > div > aside > div > div._menu_t2mh1_44 > ul > li:nth-child(1) > div', { timeout: 60000 });
-//       await page.click('#root > div > div > div > aside > div > div._menu_t2mh1_44 > ul > li:nth-child(1) > div'); // 點會員管理
-      
-//       // 等待子菜單展開
-//       await new Promise(resolve => setTimeout(resolve, 1000)); // 等待1秒鐘，確保子菜單展開
-      
-//       // 檢查每個項目是否存在
-//       const menuItems = ['新增會員', '會員清單', '新增子帳號', 'aaaaa子帳號清單'];
-//       const menuItemExists = await page.evaluate((menuItems) => {
-//         const links = document.querySelectorAll('a');
-//         const menuItemExists = {};
-//         menuItems.forEach(item => {
-//           menuItemExists[item] = false;
-//         });
-//         links.forEach(link => {
-//           const linkText = link.textContent.trim();
-//           if (menuItems.includes(linkText)) {
-//             menuItemExists[linkText] = true;
-//           }
-//         });
-//         return menuItemExists;
-//       }, menuItems);
-  
-//       // 斷言每個項目存在
-//       Object.entries(menuItemExists).forEach(([item, exists]) => {
-//         expect(exists).toBeTruthy();
-//       });
-//     });
-//   });
+
     describe('統計報表子目錄', () => {
-        test('確認輸贏報表是否存在', async () => {
+        test('點擊對帳報表', async () => {
             await page.waitForSelector('#root > div > div > div > aside > div > div._menu_t2mh1_44 > ul > li:nth-child(3) > div', { timeout: 60000 });
             await page.click('#root > div > div > div > aside > div > div._menu_t2mh1_44 > ul > li:nth-child(3) > div'); // 點統計報表
             // 等待子菜单展開
             await new Promise(resolve => setTimeout(resolve, 1000)); // 等待1秒鐘，確保子菜單展開
-            // 檢查輸贏報表是否存在
-            // const winLoseReportText = await page.evaluate(() => {
-            //     //const xpath = "/html/body/div/div/div/div/aside/div/div[2]/ul/li[3]/ul/li[1]/span/a";
-            //     const xpath = "//ul[starts-with(@id, 'rc-menu-uuid-') and contains(@id, '-report-popup')]/li[2]/span/a";
-            //     const result = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
-            //     const link = result.singleNodeValue;
-            //     return link ? link.textContent.trim() : null;
-            // });
-        
-            // // 斷言輸贏報表存在
-            // assert.strictEqual(winLoseReportText, '輸贏報表aaa');
-
-             //檢查輸贏報表是否存在
-        // const winLoseReportExists = await page.evaluate(() => {
-        //     //const xpath = "/html/body/div/div/div/div/aside/div/div[2]/ul/li[3]/ul/li[1]/span/a";
-        //     const xpath = "//ul[starts-with(@id, 'rc-menu-uuid-') and contains(@id, '-report-popup')]/li[2]/span/a";
-        //     const result = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
-        //     const link = result.singleNodeValue;
-        //     return link ? true : false;
-        // });
-    
-        // //斷言輸贏報表存在
-        // expect(winLoseReportExists).toBeTruthy();
-
             //直接點擊
         //const xpath = "/html/body/div/div/div/div/aside/div/div[2]/ul/li[3]/ul/li[1]/span/a";
         const xpath = "//ul[starts-with(@id, 'rc-menu-uuid-') and contains(@id, '-report-popup')]/li[2]/span/a";
@@ -172,7 +48,47 @@ describe.skip('會員管理子目錄', () => {
         }, xpath);
         await new Promise(resolve => setTimeout(resolve, 4000));
         });
-       
+        test('點擊玩家帳變紀錄', async () => {
+          await page.waitForSelector('#root > div > div > div > aside > div > div._menu_t2mh1_44 > ul > li:nth-child(3) > div', { timeout: 60000 });
+          //await page.click('#root > div > div > div > aside > div > div._menu_t2mh1_44 > ul > li:nth-child(3) > div'); // 點統計報表
+          // 等待子菜单展開
+          await new Promise(resolve => setTimeout(resolve, 1000)); // 等待1秒鐘，確保子菜單展開
+          //直接點擊
+      //const xpath = "/html/body/div/div/div/div/aside/div/div[2]/ul/li[3]/ul/li[1]/span/a";
+      const xpath = "//ul[starts-with(@id, 'rc-menu-uuid-') and contains(@id, '-report-popup')]/li[4]/span/a";
+      await new Promise(resolve => setTimeout(resolve, 1000));
+      await page.evaluate((xpath) => {
+          const element = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+          if (element) {
+              element.click();
+          } else {
+              throw new Error(`Element with XPath ${xpath} not found.`);
+          }
+      }, xpath);
+      await new Promise(resolve => setTimeout(resolve, 4000));
+      });
+      
+});
+describe('帳目查詢', () => {
+  test('點擊現金轉帳', async () => {
+      await page.waitForSelector('#root > div > div > div > aside > div > div._menu_t2mh1_44 > ul > li:nth-child(5) > div', { timeout: 60000 });
+      await page.click('#root > div > div > div > aside > div > div._menu_t2mh1_44 > ul > li:nth-child(5) > div'); // 點統計報表
+      // 等待子菜单展開
+      await new Promise(resolve => setTimeout(resolve, 1000)); // 等待1秒鐘，確保子菜單展開
+      //直接點擊
+  //const xpath = "/html/body/div/div/div/div/aside/div/div[2]/ul/li[3]/ul/li[1]/span/a";
+  const xpath = "//ul[starts-with(@id, 'rc-menu-uuid-') and contains(@id, '-statement-popup')]/li[1]/span/a";
+  await new Promise(resolve => setTimeout(resolve, 1000));
+  await page.evaluate((xpath) => {
+      const element = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+      if (element) {
+          element.click();
+      } else {
+          throw new Error(`Element with XPath ${xpath} not found.`);
+      }
+  }, xpath);
+  await new Promise(resolve => setTimeout(resolve, 4000));
+  });
 });
 
 
