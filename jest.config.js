a857fef402500757ee7e6de9658d28612fb62559
vic_chiu <vic_chiu@simbo.com.tw>
2024-05-28 10:39:10 +0800

    優化report檔名，加上月日分秒

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,8 @@
+const { format } = require('date-fns');
+
+// 格式化当前日期为 MMDD 格式，例如：0528
+const currentDate = format(new Date(), 'MMddmmss');
+
 module.exports = {
     reporters: [
       'default', // 默认报告器
@@ -7,7 +12,7 @@ module.exports = {
       }],
       ['jest-html-reporters', {
         publicPath: './html-report', // 输出目录
-        filename: 'report.html', // 输出文件名
+        filename: `report_${currentDate}.html`, // 输出文件名
         expand: true, // 是否展开测试结果
       }],
     ],
