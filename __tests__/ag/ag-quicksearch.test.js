efcbdc579422aaf4406ee3eb5d9eec59ac24258f
vic_chiu <vic_chiu@simbo.com.tw>
2024-06-25 11:12:20 +0800

    增加切換語系在切回來的檢查

--- __tests__/ag/ag-quicksearch.test.js
+++ __tests__/ag/ag-quicksearch.test.js
@@ -68,6 +68,25 @@ describe('檢查玩家id查詢功能', () => {
         expect(playidsearch).toBeTruthy();
         assert.equal(playidsearch, '依玩家ID查詢', 'playidsearch text is incorrect');
     })
+    test('檢查麵包屑', async () => {
+        const xpath = "//*[text()='依玩家ID查詢']";
+        await new Promise(resolve => setTimeout(resolve, 1000));
+        await page.evaluate((xpath) => {
+            const element = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+            if (element) {
+                element.click();
+            } else {
+                throw new Error(`Element with XPath ${xpath} not found.`);
+            }
+        }, xpath);
+        await new Promise(resolve => setTimeout(resolve, 1000));
+        const Breadcrumbs = await page.$eval('#root > div > div > div > div > nav > ol', element => element.textContent.trim());
+        expect(Breadcrumbs).toBeTruthy();
+        assert.equal(Breadcrumbs, '總覽/快速搜尋/依玩家ID查詢', 'Breadcrumbs text is incorrect');
+        expect(Breadcrumbs).toContain('總覽');
+        expect(Breadcrumbs).toContain('快速搜尋');
+        expect(Breadcrumbs).toContain('依玩家ID查詢');
+    })
     test('檢查輸入框預設文字', async () => {
         const xpath = "//*[text()='依玩家ID查詢']";
         await new Promise(resolve => setTimeout(resolve, 1000));
@@ -159,6 +178,135 @@ describe('檢查玩家id查詢功能', () => {
         assert.equal(userdata, 'vic0522(RMB)', 'online player element text is incorrect');
 
     })
+    test('切換語系，檢查錯誤提示', async () => {
+        const xpathForchangelanguage = '//*[@data-trigger-id="dropdown-language-i"]';
+
+        // 等待元素出现
+        await page.waitForFunction((xpath) => {
+            const result = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
+            return result.singleNodeValue !== null;
+        }, { timeout: 60000 }, xpathForchangelanguage);
+  
+  
+   // 获取 CSS 选择器
+   const cssSelector = await page.evaluate((xpath) => {
+      const element = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+      return element ? `[data-trigger-id="${element.getAttribute('data-trigger-id')}"]` : null;
+    }, xpathForchangelanguage);
+  
+    if (cssSelector) {
+        // 鼠标悬停在元素上
+        await page.hover(cssSelector);
+  
+        // 等待弹出窗口完全显示
+        await page.waitForSelector('.ant-dropdown-menu', { visible: true });
+  
+        // 等待一段时间以确保弹出窗口完全显示
+        await new Promise(resolve => setTimeout(resolve, 4000)); // 你可以调整这个等待时间
+  
+        // 检查弹窗是否存在
+        const isVisible = await page.evaluate(() => {
+          const menu = document.querySelector('.ant-dropdown-menu');
+          return menu && window.getComputedStyle(menu).display !== 'none' && menu.offsetWidth > 0 && menu.offsetHeight > 0;
+      });
+      if (!isVisible) {
+          throw new Error('Dropdown menu is not visible.');
+      }
+  
+        // 点击弹出菜单中的 '简体中文' 选项
+        const xpathForEN = "//*[text()='English']";
+        await page.waitForFunction((xpath) => {
+            const result = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
+            return result.singleNodeValue !== null;
+        }, { timeout: 60000 }, xpathForEN);
+  
+        await page.evaluate((xpath) => {
+            const element = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+            if (element) {
+                element.click();
+            } else {
+                throw new Error(`Element with XPath ${xpath} not found.`);
+            }
+        }, xpathForEN);
+  
+    } else {
+        throw new Error(`Failed to get CSS selector for element with XPath ${xpathForchangelanguage}.`);
+    }
+    await new Promise(resolve => setTimeout(resolve, 5000)); // 等待1秒鐘，確保子菜單展開
+        const xpath = "//*[text()='By Player ID']";
+        await new Promise(resolve => setTimeout(resolve, 1000));
+        await page.evaluate((xpath) => {
+            const element = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+            if (element) {
+                element.click();
+            } else {
+                throw new Error(`Element with XPath ${xpath} not found.`);
+            }
+        }, xpath);
+        await new Promise(resolve => setTimeout(resolve, 1000));
+        await page.click("#root > div > div > div > div > main > div > div:nth-child(1) > div > div > div > form > div > div > div > div > div > span > span > span.ant-input-affix-wrapper.ant-input-affix-wrapper-lg.css-1r287do.ant-input-outlined.ant-input-status-success > span > span > span > svg");
+        await page.type('#userId', 'vic032522');
+        await page.click("#root > div > div > div > div > main > div > div:nth-child(1) > div > div > div > form > div > div > div > div > div > span > span > span.ant-input-group-addon > button > span");
+        await new Promise(resolve => setTimeout(resolve, 3000));
+        const userdata = await page.$eval('#root > div > div > div > div > main > div > div:nth-child(2) > div > div > div > div > div:nth-child(1) > div > div > div:nth-child(2) > span', element => element.textContent.trim());
+        expect(userdata).toBeTruthy();
+        assert.equal(userdata, 'vic032522(RMB)', 'online player element text is incorrect');
+
+        const xpathForchangelanguageback = '//*[@data-trigger-id="dropdown-language-i"]';
+
+        // 等待元素出现
+        await page.waitForFunction((xpath) => {
+            const result = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
+            return result.singleNodeValue !== null;
+        }, { timeout: 60000 }, xpathForchangelanguageback);
+  
+  
+   // 获取 CSS 选择器
+   const cssSelectortozh = await page.evaluate((xpath) => {
+      const element = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+      return element ? `[data-trigger-id="${element.getAttribute('data-trigger-id')}"]` : null;
+    }, xpathForchangelanguageback);
+  
+    if (cssSelectortozh) {
+        // 鼠标悬停在元素上
+        await page.hover(cssSelectortozh);
+  
+        // 等待弹出窗口完全显示
+        await page.waitForSelector('.ant-dropdown-menu', { visible: true });
+  
+        // 等待一段时间以确保弹出窗口完全显示
+        await new Promise(resolve => setTimeout(resolve, 4000)); // 你可以调整这个等待时间
+  
+        // 检查弹窗是否存在
+        const isVisible = await page.evaluate(() => {
+          const menu = document.querySelector('.ant-dropdown-menu');
+          return menu && window.getComputedStyle(menu).display !== 'none' && menu.offsetWidth > 0 && menu.offsetHeight > 0;
+      });
+      if (!isVisible) {
+          throw new Error('Dropdown menu is not visible.');
+      }
+  
+        // 点击弹出菜单中的 '中文' 选项
+        const xpathForCN = "//*[text()='繁體中文']";
+        await page.waitForFunction((xpath) => {
+            const result = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
+            return result.singleNodeValue !== null;
+        }, { timeout: 60000 }, xpathForCN);
+  
+        await page.evaluate((xpath) => {
+            const element = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+            if (element) {
+                element.click();
+            } else {
+                throw new Error(`Element with XPath ${xpath} not found.`);
+            }
+        }, xpathForCN);
+  
+    } else {
+        throw new Error(`Failed to get CSS selector for element with XPath ${xpathForchangelanguageback}.`);
+    }
+    await new Promise(resolve => setTimeout(resolve, 5000)); // 等待1秒鐘，確保子菜單展開
+    })
     test('不輸入玩家id，檢查錯誤提示', async () => {
         const xpath = "//*[text()='依玩家ID查詢']";
         await new Promise(resolve => setTimeout(resolve, 1000));
@@ -176,7 +324,7 @@ describe('檢查玩家id查詢功能', () => {
         await new Promise(resolve => setTimeout(resolve, 3000));
         const noinputuserdata = await page.$eval('#userId_help > div', element => element.textContent.trim());
         expect(noinputuserdata).toBeTruthy();
-        assert.equal(noinputuserdata, 'vic0522(RMB)', 'online player element text is incorrect');
+        assert.equal(noinputuserdata, '請輸入玩家ID', 'online player element text is incorrect');
 
     })
 });
@@ -338,6 +486,80 @@ describe('檢查單號查詢功能', () => {
         const noentermsg = await page.$eval('#userId_help > div', element => element.textContent.trim());
         expect(noentermsg).toBeTruthy();
         assert.equal(noentermsg, '此欄位為必填', 'noentermsg text is incorrect');
+    });
+    test('切換語系，檢查錯誤提示', async () => {
+        const xpathForchangelanguage = '//*[@data-trigger-id="dropdown-language-i"]';
+
+        // 等待元素出现
+        await page.waitForFunction((xpath) => {
+            const result = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
+            return result.singleNodeValue !== null;
+        }, { timeout: 60000 }, xpathForchangelanguage);
+  
+  
+   // 获取 CSS 选择器
+   const cssSelector = await page.evaluate((xpath) => {
+      const element = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+      return element ? `[data-trigger-id="${element.getAttribute('data-trigger-id')}"]` : null;
+    }, xpathForchangelanguage);
+  
+    if (cssSelector) {
+        // 鼠标悬停在元素上
+        await page.hover(cssSelector);
+  
+        // 等待弹出窗口完全显示
+        await page.waitForSelector('.ant-dropdown-menu', { visible: true });
+  
+        // 等待一段时间以确保弹出窗口完全显示
+        await new Promise(resolve => setTimeout(resolve, 4000)); // 你可以调整这个等待时间
+  
+        // 检查弹窗是否存在
+        const isVisible = await page.evaluate(() => {
+          const menu = document.querySelector('.ant-dropdown-menu');
+          return menu && window.getComputedStyle(menu).display !== 'none' && menu.offsetWidth > 0 && menu.offsetHeight > 0;
+      });
+      if (!isVisible) {
+          throw new Error('Dropdown menu is not visible.');
+      }
+  
+        // 点击弹出菜单中的 '简体中文' 选项
+        const xpathForCN = "//*[text()='English']";
+        await page.waitForFunction((xpath) => {
+            const result = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
+            return result.singleNodeValue !== null;
+        }, { timeout: 60000 }, xpathForCN);
+  
+        await page.evaluate((xpath) => {
+            const element = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+            if (element) {
+                element.click();
+            } else {
+                throw new Error(`Element with XPath ${xpath} not found.`);
+            }
+        }, xpathForCN);
+  
+    } else {
+        throw new Error(`Failed to get CSS selector for element with XPath ${xpathForchangelanguage}.`);
+    }
+    await new Promise(resolve => setTimeout(resolve, 5000)); // 等待1秒鐘，確保子菜單展開
+        const xpath = "//*[text()='By Sequence ID']";
+        await new Promise(resolve => setTimeout(resolve, 1000));
+        await page.evaluate((xpath) => {
+            const element = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+            if (element) {
+                element.click();
+            } else {
+                throw new Error(`Element with XPath ${xpath} not found.`);
+            }
+        }, xpath);
+        await new Promise(resolve => setTimeout(resolve, 1000));
+        await page.click("#root > div > div > div > div > main > div > div:nth-child(1) > div > div > div > form > div > div.ant-row.ant-form-item-row.css-1r287do > div > div.ant-form-item-control-input > div > span > span > span.ant-input-affix-wrapper.ant-input-affix-wrapper-lg.css-1r287do.ant-input-outlined.ant-input-status-error > span > span > span > svg > path");
+        await page.type('#userId', '');
+        await page.click("#root > div > div > div > div > main > div > div:nth-child(1) > div > div > div > form > div > div > div > div > div > span > span > span.ant-input-group-addon > button > span");
+        await new Promise(resolve => setTimeout(resolve, 3000));
+        const noentermsg = await page.$eval('#userId_help > div', element => element.textContent.trim());
+        expect(noentermsg).toBeTruthy();
+        assert.equal(noentermsg, 'Column is required.', 'noentermsg text is incorrect');
     });
 });
 
