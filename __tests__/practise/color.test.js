58efa2b8ae2027a059681245e9b28f8312c49456
vic_chiu <vic_chiu@simbo.com.tw>
2024-07-19 15:17:58 +0800

    調整登入帳號

--- __tests__/practise/color.test.js
+++ __tests__/practise/color.test.js
@@ -20,7 +20,7 @@ beforeAll(async () => {
     await page.waitForSelector('#root > div > div > div > div > form > div > div:nth-child(4) > button', { timeout: 60000 });
 
     // 登入
-    await page.type('#account', 'vicag');
+    await page.type('#account', 'admin');
     await page.type('#password', 'aaaa1234');
     await page.click("#root > div > div > div > div > form > div > div:nth-child(4) > button");
     await page.waitForSelector('#root > div > div > header > div._icon_iyfbn_38.ml-auto.flex.items-center.justify-center > div.ml-4.cursor-pointer.font-bold > button > span.ml-1', { timeout: 60000 });
@@ -30,7 +30,7 @@ afterAll(async () => {
 });
 
 describe('dashboard 數字檢查', () => {
-    test('檢查負數顏色', async () => {
+    test('檢查負數顏色(紅色)', async () => {
         const selector = '#root > div > div > div > div > main > div.ant-row.ant-row-center.css-1r287do > div:nth-child(2) > div > div > div > h2.text-3xl.my-0 > span';
         
         await page.waitForSelector('#root > div > div > div > div > main > div.ant-row.ant-row-center.css-1r287do > div:nth-child(2) > div > div > div > h2.text-3xl.my-0 > span', { timeout: 60000 });
@@ -49,7 +49,7 @@ describe('dashboard 數字檢查', () => {
         // 驗證顏色是否為預期值
         expect(color).toBe('rgb(230, 29, 88)'); // 使用 Jest 斷言來檢查顏色
     });
-    test('檢查正數顏色色碼版本', async () => {
+    test('檢查正數顏色色碼版本（藍色）', async () => {
         const selector = '#root > div > div > div > div > main > div.ant-row.ant-row-center.css-1r287do > div:nth-child(1) > div > div > div > h2.text-3xl.my-0 > span';
         
         await page.waitForSelector(selector);
@@ -95,4 +95,23 @@ describe('dashboard 數字檢查', () => {
         // 驗證顏色是否為預期值
         expect(color).toBe(expectedColor); // 使用 Jest 斷言來檢查顏色
     });
+    test('檢查數字顏色（紅色或藍色）', async () => {
+        const selector = '#root > div > div > div > div > main > div.ant-row.ant-row-center.css-1r287do > div:nth-child(2) > div > div > div > h2.text-3xl.my-0 > span';
+        
+        await page.waitForSelector(selector, { timeout: 60000 });
+        await new Promise(resolve => setTimeout(resolve, 5000));
+        
+        // 獲取元素的顏色屬性
+        const color = await page.evaluate((selector) => {
+            const element = document.querySelector(selector);
+            return window.getComputedStyle(element).color;
+        }, selector);
+
+        console.log(color); // 輸出顏色屬性值，例如 "rgb(230, 29, 88)"
+
+        // 驗證顏色是否為預期值
+        const expectedColors = ['rgb(230, 29, 88)', 'rgb(64, 158, 255)'];
+        expect(expectedColors).toContain(color); // 使用 Jest 斷言來檢查顏色是否在預期值中
+    });
 });
+
