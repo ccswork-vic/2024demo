5a3346bbe888dab60d1875e1b46b98306c13cd34
vic_chiu <vic_chiu@simbo.com.tw>
2024-11-20 10:01:17 +0800

    更新檔名加上ux後綴，新增測項

--- __tests__/botan-casino/wallet-ux.test.js
+++ __tests__/botan-casino/wallet-ux.test.js
@@ -40,7 +40,7 @@ describe('檢查wallet', () => {
     }, xpath);
     expect(xpath).toBeTruthy(); // 检查按钮是否存在
   });
-  test('檢查左側清單存在帳戶管理', async () => {
+  test.skip('檢查左側清單存在帳戶管理', async () => {
     //await page.goto('https://dev.botan888.co/wallet/balance', { waitUntil: "domcontentloaded" });
     const xpath = "//*[text()='การจัดการบัญชี']";
     await new Promise(resolve => setTimeout(resolve, 1000));
@@ -81,8 +81,58 @@ describe('檢查wallet', () => {
   test('檢查該頁面標題', async () => {
     await page.waitForSelector('.flex.items-center.gap-2.text-lg', { timeout: 60000 });
     const WelcomeText = await page.$eval('.flex.items-center.gap-2.text-lg', element => element.textContent.trim());
-    assert.equal(WelcomeText, 'บัญชีคุณaaaa', 'Admin setting element text is incorrect');
+    assert.equal(WelcomeText, 'กระเป๋า', 'Admin setting element text is incorrect');
       // 等待彈窗並加上時間讓他完全顯示
     await new Promise(resolve => setTimeout(resolve, 2000)); 
   });
+  // test('檢查該頁面標題-版本2', async () => {
+  //   const xpath = '//div[contains(@class, "flex items-center gap-2 text-lg")]';
+
+  //   // 等待元素出現
+  //   await page.waitForXPath(xpath, { timeout: 60000 });
+    
+  //   // 使用 XPath 查找元素
+  //   const [elementHandle] = await page.$x(xpath);
+  //   const WelcomeText = await page.evaluate(element => element.textContent.trim(), elementHandle);
+    
+  //   // 驗證文字內容
+  //   assert.equal(WelcomeText, 'บัญชีคุณaaaa', 'Admin setting element text is incorrect');
+  // });
+  test('檢查左側清單存在投注記錄', async () => {
+    //await page.goto('https://dev.botan888.co/wallet/balance', { waitUntil: "domcontentloaded" });
+    const xpath = "//*[text()='ประวัติการเดิมพัน']";
+    await new Promise(resolve => setTimeout(resolve, 1000));
+    await page.evaluate((xpath) => {
+        const element = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+        if (!element) {
+          throw new Error(`Element with XPath ${xpath} not found.`);
+        }
+    }, xpath);
+    expect(xpath).toBeTruthy(); // 检查按钮是否存在
+  });
+  test('檢查左側清單存在交易記錄', async () => {
+    //await page.goto('https://dev.botan888.co/wallet/balance', { waitUntil: "domcontentloaded" });
+    const xpath = "//*[text()='ประวัติการทำธุรกรรม']";
+    await new Promise(resolve => setTimeout(resolve, 1000));
+    await page.evaluate((xpath) => {
+        const element = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+        if (!element) {
+          throw new Error(`Element with XPath ${xpath} not found.`);
+        }
+    }, xpath);
+    expect(xpath).toBeTruthy(); // 检查按钮是否存在
+  });
+  test('檢查左側清單存在派獎記錄', async () => {
+    //await page.goto('https://dev.botan888.co/wallet/balance', { waitUntil: "domcontentloaded" });
+    const xpath = "//*[text()='ประวัติการรับโบนัส']";
+    await new Promise(resolve => setTimeout(resolve, 1000));
+    await page.evaluate((xpath) => {
+        const element = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+        if (!element) {
+          throw new Error(`Element with XPath ${xpath} not found.`);
+        }
+    }, xpath);
+    expect(xpath).toBeTruthy(); // 检查按钮是否存在
+  });
+  
 });
