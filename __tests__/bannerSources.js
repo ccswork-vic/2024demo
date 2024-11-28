a0e5f68465500a31975dab6e1ea73c2a44d171c6
vic_chiu <vic_chiu@simbo.com.tw>
2024-11-28 09:48:25 +0800

    更新首頁banner 路徑

--- __tests__/bannerSources.js
+++ __tests__/bannerSources.js
@@ -5,20 +5,18 @@ module.exports = {
       "https://dev.botan888.co/images/Promotion_seconddepositbonus_TH_900_v2.webp",//二儲
       "https://dev.botan888.co/images/Promotion_bonusdaily10_TH_900_v2.jpg",//天天儲值
       "https://dev.botan888.co/images/Promotion_wednesday-bonus_TH_900.jpg",//週三
-      "/assets/banner1-DvwNp99997.jpg",//測試找不到圖片報錯用
-      "/assets/banner1-DvwNp4G7.jpg",
-      "/assets/banner2-BOuwdAAc.jpg",
-      "/assets/banner3-yN9At2xV.jpg",
-      "/assets/banner4-BnHaGrm7.jpg",
-      "/assets/banner5-JhufrqdN.jpg",
-      "/assets/banner6-sWPQJEWS.jpg",
+      //"/assets/banner1-DvwNp99997.jpg",//測試找不到圖片報錯用
+      "/assets/Promotion_VIP_TH-DvwNp4G7.jpg",
+      "/assets/Promotion_weeklybonus2_TH-BOuwdAAc.jpg",
+      "/assets/Promotion_luckywheel_TH-yN9At2xV.jpg",
+      "/assets/Promotion_invitebonus_TH-BnHaGrm7.jpg",
+      "/assets/Promotion_login7days_TH-sWPQJEWS.jpg",
     ],
     bannerAlts: [
       "สมัครสมาชิก รับ 1,000",
       "โปรโมชั่นฝากเงินครั้งแรก",
       "ฝากครั้งที่สอง รับโบนัส 50% เพิ่มโอกาสชนะ",
       "รับเงินรางวัล $150,000 ทุกสัปดาห์!",
-      "vic",
     ],
     socialMedia: [
       "facebook",
