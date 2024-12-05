4b12be18cc66d668b91c5df5e535c1c50be9c6df
vic_chiu <vic_chiu@simbo.com.tw>
2024-12-05 12:21:55 +0800

    更新jest.config

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 const { format } = require('date-fns');
 
-// 格式化当前日期为 MMDD 格式，例如：0528
+// 格式化當前日期為 MMDD 格式，例如：0528
 const currentDate = format(new Date(), 'MMddmmss');
 
 module.exports = {
@@ -14,6 +14,10 @@ module.exports = {
         publicPath: './html-report', // 输出目录
         filename: `report_${currentDate}.html`, // 输出文件名
         expand: true, // 是否展开测试结果
+      //   customInfos: {
+      //     Version: process.env.PROJECT_VERSION || '未知版本', // 顯示版本號
+      // },
       }],
     ],
+    globalSetup: './setupVersion.js', // 新增 globalSetup 配置
   };
